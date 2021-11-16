dac93c368ecd2897406edc5b2ab5a2a1bfc8e718
Jean Luc <jeanassamoi98@yahoo.com>
2021-11-16 22:58:01 +0100

    employee manager v1

---
+++ formationjava/springworkspace/employeemanagerbackend/src/main/java/com/askam/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.askam.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+
+	
+}
---
+++ formationjava/springworkspace/employeemanagerbackend/src/main/java/com/askam/employeemanager/model/Employee.java
@@ -0,0 +1,30 @@
+package com.askam.employeemanager.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+  	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}
